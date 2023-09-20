a811800740a949fee4a624f6d19c60d40c2a0143
hamza elbakkouri <112905780+hamzaelbak66@users.noreply.github.com>
2023-09-20 19:59:16 +0100

    first push

---
+++ src/DTO/Person.java
@@ -0,0 +1,10 @@
+package DTO;
+
+import java.time.LocalDate;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private LocalDate dateOfBirth;
+    private String phoneNumber;
+}
